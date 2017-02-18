c139c5038d9926e6e21ba2692fd7e8a373777095
Bob Krijnen <krijnenbob@live.nl>
2017-02-18 16:20:50 +0000

    add AssetsMiddleware

---
+++ src/Middleware/AssetsMiddleware.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Rokr\Tracer\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+
+class AssetsMiddleware
+{
+    public function handle(Request $request, Closure $next)
+    {
+    	$response = $next($request);
+    	$content = $response->content();
+
+    	$content = str_replace(
+    		'</head>', 
+    		'<script type="text/javascript" src="js/tracer.js"></script></head>', 
+    		$content
+    	);
+
+    	return $response->setContent($content);
+    }
+}
