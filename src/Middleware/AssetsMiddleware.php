a1f78d19aabaf1c8249486ace3d633ec12792078
Gijs Jorissen <gizburdt@gmail.com>
2017-02-23 17:20:44 +0000

    Apply fixes from StyleCI

--- src/Middleware/AssetsMiddleware.php
+++ src/Middleware/AssetsMiddleware.php
@@ -4,23 +4,22 @@ namespace Appstract\Tracer\Middleware;
 
 use Closure;
 use Illuminate\Http\Request;
-use Illuminate\Http\Response;
 
 class AssetsMiddleware
 {
     public function handle(Request $request, Closure $next)
     {
-    	$response = $next($request);
-    	$content = $response->content();
+        $response = $next($request);
+        $content = $response->content();
 
-    	$content = str_replace(
-    		'</head>',
-    		'<link href="/css/laravel-tracer.css" rel="stylesheet">
+        $content = str_replace(
+            '</head>',
+            '<link href="/css/laravel-tracer.css" rel="stylesheet">
              <script type="text/javascript" src="/js/laravel-tracer.js"></script>
              </head>',
-    		$content
-    	);
+            $content
+        );
 
-    	return $response->setContent($content);
+        return $response->setContent($content);
     }
 }
