4d3dde6edb777b89f2615c09be5ced3f7251b4b7
Olav van Schie <olavschie@hotmail.com>
2017-03-09 22:54:22 +0100

    some styling

--- assets/js/laravel-tracer.js
+++ assets/js/laravel-tracer.js
@@ -1,23 +1,22 @@
 document.addEventListener("DOMContentLoaded", function(e) {
 
-	function toggleTrace() {
-		var traces = document.getElementsByClassName('laravel-trace');
+    function toggleTrace() {
+        var traces = document.getElementsByClassName('laravel-trace');
 
-		for(var i = 0; i < traces.length; i++){
+        for(var i = 0; i < traces.length; i++){
+            if (traces[i].classList.contains('hidden')) {
+                traces[i].classList.remove('hidden');
+            } else {
+                traces[i].classList.add('hidden');
+            }
+        }
+    }
 
-			if (traces[i].style.display === 'none') {
-				traces[i].style.display = 'block';
-			}else{
-				traces[i].style.display = 'none';
-			}
-		}
-	}
+    function KeyPress(e) {
+        var evtobj = window.event? event : e
+        if (evtobj.keyCode == 90 && evtobj.ctrlKey) toggleTrace();
+    }
 
-	function KeyPress(e) {
-	      var evtobj = window.event? event : e
-	      if (evtobj.keyCode == 90 && evtobj.ctrlKey) toggleTrace();
-	}
-
-	document.onkeydown = KeyPress;
+    document.onkeydown = KeyPress;
 
 });
